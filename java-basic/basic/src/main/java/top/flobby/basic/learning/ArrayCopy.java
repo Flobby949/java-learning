f5993439d1d2004f02e6b6109526a7bf95c1ffe7
jinchenxing <2541226493@qq.com>
2021-11-22 09:46:50 +0800

    异常处理

--- java-basic/basic/src/main/java/top/flobby/basic/learning/ArrayCopy.java
+++ java-basic/basic/src/main/java/top/flobby/basic/learning/ArrayCopy.java
@@ -13,6 +13,13 @@ import java.util.Arrays;
 
 public class ArrayCopy {
     public static void main(String[] args) {
+        Integer i1 = 100;
+        Integer i2 = 100;
+        Integer i3 = 200;
+        Integer i4 = 200;
+
+        System.out.println(i1 == i2);
+        System.out.println(i3 == i4);
         System.out.println("****************1*****************");
         copySelf();
         System.out.println("****************2*****************");
